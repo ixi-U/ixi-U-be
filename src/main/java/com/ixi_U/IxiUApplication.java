830b279510f0a7d9848275119ff2323592f060d8
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-06-07 14:03:45 +0900

    feat: project init

---
+++ src/main/java/com/ixi_U/IxiUApplication.java
@@ -0,0 +1,13 @@
+package com.ixi_U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxiUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IxiUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ixi_U/IxiUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ixi_U;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IxiUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
