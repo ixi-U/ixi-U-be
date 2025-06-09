65ee3127ef3ee5d4649ab4d8c97fa562d3f0e63a
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-09 15:46:01 +0900

    feat : SCRUM-21 사용자의 ID와 선택한 요금제의 ID를 받아서 사용자의 현재 요금제를 업데이트 하는 Controller 추가

---
+++ src/main/java/com/ixi_U/user/controller/SubscribedController.java
@@ -0,0 +1,29 @@
+package com.ixi_U.user.controller;
+
+import com.ixi_U.user.dto.CreateSubscribedRequest;
+import com.ixi_U.user.service.SubscribedService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subscribed")
+public class SubscribedController {
+
+    private final SubscribedService subscribedService;
+
+    public SubscribedController(SubscribedService subscribedService) {
+        this.subscribedService = subscribedService;
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<Void> updateSubscribe(
+            @PathVariable("userId") String userId,
+            @RequestBody CreateSubscribedRequest request) {
+        subscribedService.registerSubscribed(userId, request);
+        return ResponseEntity.ok().build();
+    }
+}
