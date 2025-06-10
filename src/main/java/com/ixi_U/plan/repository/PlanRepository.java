ddeca22556e146021c91ccd1feb3411d74164c9b
yereumi <lovelyyeji01@gmail.com>
2025-06-10 10:52:00 +0900

    feat: SCRUM-14 PlanRepository 인터페이스 추가

---
+++ src/main/java/com/ixi_U/plan/repository/PlanRepository.java
@@ -0,0 +1,10 @@
+package com.ixi_U.plan.repository;
+
+import com.ixi_U.plan.entity.Plan;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanRepository extends Neo4jRepository<Plan, String>, PlanCustomRepository {
+
+}
