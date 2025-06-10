b72f6e2c6fa0123765947177eb28ffd092b0b6b5
yereumi <lovelyyeji01@gmail.com>
2025-06-10 10:53:53 +0900

    feat: SCRUM-14 PlanController 요금제 정렬/검색 API 추가

---
+++ src/main/java/com/ixi_U/plan/Controller/PlanController.java
@@ -0,0 +1,35 @@
+package com.ixi_U.plan.Controller;
+
+import com.ixi_U.plan.dto.response.SortedPlanResponse;
+import com.ixi_U.plan.service.PlanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/plans")
+@RequiredArgsConstructor
+public class PlanController {
+
+    private final PlanService planService;
+
+    @GetMapping
+    public ResponseEntity<SortedPlanResponse> getPlans(@RequestParam(defaultValue = "10") int size,
+            @RequestParam String planTypeStr,
+            @RequestParam(defaultValue = "PRIORITY") String planSortOptionStr,
+            @RequestParam(required = false) String searchKeyword,
+            @RequestParam(required = false) String planId,
+            @RequestParam(required = false) Integer cursorSortValue) {
+
+        Pageable pageable = PageRequest.ofSize(size);
+        SortedPlanResponse response = planService.findPlans(pageable, planTypeStr,
+                planSortOptionStr, searchKeyword, planId, cursorSortValue);
+
+        return ResponseEntity.ok(response);
+    }
+}
