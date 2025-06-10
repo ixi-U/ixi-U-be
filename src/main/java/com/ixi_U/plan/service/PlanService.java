730a9a4d4dc79d2786c87b68a4471bec720d2aa9
yereumi <lovelyyeji01@gmail.com>
2025-06-10 10:53:17 +0900

    feat: SCRUM-14 PlanService 요금제 정렬/검색 서비스 로직 구현

---
+++ src/main/java/com/ixi_U/plan/service/PlanService.java
@@ -0,0 +1,68 @@
+package com.ixi_U.plan.service;
+
+import com.ixi_U.common.exception.GeneralException;
+import com.ixi_U.plan.dto.PlanSummaryDto;
+import com.ixi_U.plan.dto.response.SortedPlanResponse;
+import com.ixi_U.plan.entity.PlanSortOption;
+import com.ixi_U.plan.entity.PlanType;
+import com.ixi_U.plan.exception.PlanException;
+import com.ixi_U.plan.repository.PlanRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlanService {
+
+    private final PlanRepository planRepository;
+
+    public SortedPlanResponse findPlans(Pageable pageable, String planTypeStr, String sortOptionStr,
+            String query, String planId, Integer sortValue) {
+
+        PlanType planType = PlanType.from(planTypeStr);
+        PlanSortOption planSortOption = PlanSortOption.from(sortOptionStr);
+        Slice<PlanSummaryDto> plans = planRepository.findPlans(pageable, planType, planSortOption,
+                query, planId, sortValue);
+
+        String lastPlanId = getLastPlanId(plans);
+        int lastSortValue = getLastSortValue(plans, planSortOption);
+
+        return new SortedPlanResponse(plans, lastPlanId, lastSortValue);
+    }
+
+    private int extractSortValue(PlanSummaryDto planSummary, PlanSortOption sortOption) {
+
+        return switch (sortOption.getField()) {
+            case "priority" -> planSummary.priority();
+            case "monthlyPrice" -> planSummary.monthlyPrice();
+            case "mobileDataLimitMb" -> planSummary.mobileDataLimitMb();
+            default -> throw new GeneralException(PlanException.INVALID_SORT_VALUE);
+        };
+    }
+
+    private String getLastPlanId(Slice<PlanSummaryDto> plans) {
+
+        if (!plans.hasNext()) {
+
+            return null;
+        }
+        List<PlanSummaryDto> content = plans.getContent();
+
+        return content.get(content.size() - 1).id();
+    }
+
+    private int getLastSortValue(Slice<PlanSummaryDto> plans, PlanSortOption sortOption) {
+
+        if (!plans.hasNext()) {
+
+            return 0;
+        }
+        List<PlanSummaryDto> content = plans.getContent();
+        PlanSummaryDto last = content.get(content.size() - 1);
+
+        return extractSortValue(last, sortOption);
+    }
+}
