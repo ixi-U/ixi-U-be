3cc02cb57d6444140d46567527f010799d7c9745
yereumi <lovelyyeji01@gmail.com>
2025-06-10 16:25:37 +0900

    feat: SCRUM-14 extractSortValue 메서드 위치 수정

--- src/main/java/com/ixi_U/plan/entity/PlanSortOption.java
+++ src/main/java/com/ixi_U/plan/entity/PlanSortOption.java
@@ -1,36 +1,47 @@
 package com.ixi_U.plan.entity;
 
 import com.ixi_U.common.exception.GeneralException;
+import com.ixi_U.plan.dto.PlanSummaryDto;
 import com.ixi_U.plan.exception.PlanException;
 import lombok.Getter;
 
 @Getter
 public enum PlanSortOption {
 
-	PRIORITY("priority", SortOrder.DESC, Comparison.LT),
-	PRICE_ASC("monthlyPrice", SortOrder.ASC, Comparison.GT),
-	PRICE_DESC("monthlyPrice", SortOrder.DESC, Comparison.LT),
-	DATA_DESC("mobileDataLimitMb", SortOrder.DESC, Comparison.LT);
+    PRIORITY("priority", SortOrder.DESC, Comparison.LT),
+    PRICE_ASC("monthlyPrice", SortOrder.ASC, Comparison.GT),
+    PRICE_DESC("monthlyPrice", SortOrder.DESC, Comparison.LT),
+    DATA_DESC("mobileDataLimitMb", SortOrder.DESC, Comparison.LT);
 
-	public final String field;
-	public final SortOrder order;
-	public final Comparison comparison;
+    public final String field;
+    public final SortOrder order;
+    public final Comparison comparison;
 
-	PlanSortOption(String field, SortOrder order, Comparison comparison) {
+    PlanSortOption(String field, SortOrder order, Comparison comparison) {
 
-		this.field = field;
-		this.order = order;
-		this.comparison = comparison;
-	}
+        this.field = field;
+        this.order = order;
+        this.comparison = comparison;
+    }
 
-	public static PlanSortOption from(String key) {
+    public static PlanSortOption from(String planSortOptionStr) {
 
-		return switch (key.toLowerCase()) {
-			case "priority" -> PRIORITY;
-			case "priceasc" -> PRICE_ASC;
-			case "pricedesc" -> PRICE_DESC;
-			case "mobiledatalimitmb" -> DATA_DESC;
-			default -> throw new GeneralException(PlanException.INVALID_SORT_VALUE);
-		};
-	}
+        return switch (planSortOptionStr) {
+            case "PRIORITY" -> PRIORITY;
+            case "PRICE_ASC" -> PRICE_ASC;
+            case "PRICE_DESC" -> PRICE_DESC;
+            case "DATA_DESC" -> DATA_DESC;
+            default -> throw new GeneralException(PlanException.INVALID_SORT_VALUE);
+        };
+    }
+
+    public static int extractSortValue(PlanSummaryDto planSummary, PlanSortOption sortOption) {
+
+        return switch (sortOption.getField()) {
+            case "priority" -> planSummary.priority();
+            case "monthlyPrice" -> planSummary.monthlyPrice();
+            case "mobileDataLimitMb" -> planSummary.mobileDataLimitMb();
+            default -> throw new GeneralException(PlanException.INVALID_SORT_VALUE);
+        };
+    }
 }
