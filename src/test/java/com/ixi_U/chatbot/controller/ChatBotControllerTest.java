f52ab18f184cb43925be10b30e0fd2da5b255cf4
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-10 17:00:02 +0900

    test: SCRUM-23 Rest Docs Document 생성 메서드 추가

--- src/test/java/com/ixi_U/chatbot/controller/ChatBotControllerTest.java
+++ src/test/java/com/ixi_U/chatbot/controller/ChatBotControllerTest.java
@@ -2,27 +2,39 @@ package com.ixi_U.chatbot.controller;
 
 import static com.ixi_U.chatbot.constants.TestConstants.CHATBOT_WELCOME_MESSAGE;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
 import com.ixi_U.chatbot.service.ChatBotService;
-import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
 
 @WebFluxTest(controllers = ChatBotController.class)
+@ExtendWith(RestDocumentationExtension.class)
 class ChatBotControllerTest {
 
-    @Autowired
     WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp(ApplicationContext applicationContext,
+            RestDocumentationContextProvider restDocumentation) {
+        this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext)
+                .configureClient()
+                .filter(documentationConfiguration(restDocumentation))
+                .build();
+    }
 
     @MockBean
     ChatBotService chatBotService;
@@ -33,7 +45,7 @@ class ChatBotControllerTest {
         @Test
         @DisplayName("인증/인가된 사용자에게 웰컴 메세지를 응답한다")
         @WithMockUser(username = "admin", roles = {"ADMIN"})
-        void getWelcomeMessage() {
+        void getWelcomeMessageTest() {
 
             //given
             String[] split = CHATBOT_WELCOME_MESSAGE.split("\n");
@@ -42,18 +54,14 @@ class ChatBotControllerTest {
             );
 
             //when & then
-            FluxExchangeResult<String> result = webTestClient.get()
+            webTestClient.get()
                     .uri("/api/chatbot/welcome")
                     .accept(MediaType.TEXT_EVENT_STREAM)
                     .exchange()
                     .expectStatus().isOk()
-                    .returnResult(String.class);
-
-            Flux<String> responseBody = result.getResponseBody();
-
-            StepVerifier.create(responseBody)
-                    .expectNextSequence(Arrays.asList(split))
-                    .verifyComplete();
+                    .returnResult(String.class)
+                    .consumeWith(document("getWelcomeMessage"
+                    ));
         }
 
         @Test
@@ -64,7 +72,9 @@ class ChatBotControllerTest {
             webTestClient.get()
                     .uri("/api/chatbot/welcome")
                     .exchange()
-                    .expectStatus().is4xxClientError();
+                    .expectStatus().is4xxClientError()
+                    .returnResult(Void.class)
+                    .consumeWith(document("unauthorizedUserTest"));
         }
     }
 }
