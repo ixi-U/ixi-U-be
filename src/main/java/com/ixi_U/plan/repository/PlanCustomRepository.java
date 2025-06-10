8193a7065787ca1063ab5daa01a14c3b2e466e17
yereumi <lovelyyeji01@gmail.com>
2025-06-10 10:52:51 +0900

    feat: SCRUM-14 CypherDSL 기반 요금제 커서 페이징 및 검색 쿼리 구현

---
+++ src/main/java/com/ixi_U/plan/repository/PlanCustomRepository.java
@@ -0,0 +1,16 @@
+package com.ixi_U.plan.repository;
+
+import com.ixi_U.plan.dto.PlanSummaryDto;
+import com.ixi_U.plan.entity.PlanSortOption;
+import com.ixi_U.plan.entity.PlanType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanCustomRepository {
+
+    Slice<PlanSummaryDto> findPlans(Pageable pageable, PlanType planType, PlanSortOption sortOption,
+            String query, String planId, Integer lastSortValue);
+
+}
