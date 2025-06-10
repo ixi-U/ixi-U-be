2f9e1cb9de5494f4e605a83b1f8d8ca0fdb32280
yereumi <lovelyyeji01@gmail.com>
2025-06-10 10:51:06 +0900

    feat: SCRUM-14 PlanException 추가

---
+++ src/main/java/com/ixi_U/plan/exception/PlanException.java
@@ -0,0 +1,22 @@
+package com.ixi_U.plan.exception;
+
+import com.ixi_U.common.exception.BaseException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PlanException implements BaseException {
+
+    INVALID_SORT_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 정렬 조건입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getExceptionName() {
+
+        return this.name();
+    }
+}
