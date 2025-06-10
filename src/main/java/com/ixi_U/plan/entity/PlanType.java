b4640f5eeacd9d0a9b082f95b2cb6a1eabc51105
yereumi <lovelyyeji01@gmail.com>
2025-06-10 10:47:10 +0900

    feat: SCRUM-14 요금제 타입 추가

--- src/main/java/com/ixi_U/plan/entity/PlanType.java
+++ src/main/java/com/ixi_U/plan/entity/PlanType.java
@@ -10,10 +10,21 @@ public enum PlanType {
     TABLET_SMARTWATCH("TABLET/SMARTWATCH"),
     DUAL_NUMBER("DUAL_NUMBER");
 
-    private final String plan_type;
+    private final String planType;
 
-    PlanType(String plan_type) {
+    PlanType(String planType) {
 
-        this.plan_type = plan_type;
+        this.planType = planType;
+    }
+
+    public static PlanType from(String type) {
+
+        return switch (type) {
+            case "5G/LTE" -> FIVE_G_LTE;
+            case "ONLINE" -> ONLINE;
+            case "TABLET/SMARTWATCH" -> TABLET_SMARTWATCH;
+            case "DUAL_NUMBER" -> DUAL_NUMBER;
+            default -> throw new IllegalArgumentException("Invalid sort option");
+        };
     }
 }
