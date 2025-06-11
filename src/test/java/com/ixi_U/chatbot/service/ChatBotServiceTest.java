855a5533c70c9abdb0ab2ec0d56a8c418f7d2fc5
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-11 13:41:46 +0900

    feat: SCRUM-32 @ActiveProfiles("test") 추가

--- src/test/java/com/ixi_U/chatbot/service/ChatBotServiceTest.java
+++ src/test/java/com/ixi_U/chatbot/service/ChatBotServiceTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class ChatBotServiceTest {
 
