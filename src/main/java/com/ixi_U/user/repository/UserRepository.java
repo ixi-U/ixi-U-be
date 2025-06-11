417295e6c6a8d8c6ee460e26bac391f291a786d3
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-11 16:53:39 +0900

    refactor : SCRUM-21 @Repository 어노테이션 추가

--- src/main/java/com/ixi_U/user/repository/UserRepository.java
+++ src/main/java/com/ixi_U/user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.ixi_U.user.repository;
 
 import com.ixi_U.user.entity.User;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends Neo4jRepository<User, String> {
 
 }
