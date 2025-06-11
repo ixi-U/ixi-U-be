6631df2cc89927b40b71957cf009675957f420ed
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-11 17:13:07 +0900

    refactor : SCRUM-21 검증에 걸릴 경우 예외 상황을 처리하기 위한 메세지 포함

--- src/main/java/com/ixi_U/user/dto/CreateSubscribedRequest.java
+++ src/main/java/com/ixi_U/user/dto/CreateSubscribedRequest.java
@@ -2,9 +2,6 @@ package com.ixi_U.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CreateSubscribedRequest(
-        @NotBlank
-        String planId
-) {
+public record CreateSubscribedRequest(@NotBlank(message = "유효하지 않은 ID") String planId) {
 
 }
