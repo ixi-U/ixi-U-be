19fba76a963ca6fbab539a7567382362e8661574
yereumi <lovelyyeji01@gmail.com>
2025-06-11 17:25:12 +0900

    feat: SCRUM-14 요금제 조회 메서드에서 page 파라미터 예외 추가

--- src/main/java/com/ixi_U/plan/controller/PlanController.java
+++ src/main/java/com/ixi_U/plan/controller/PlanController.java
@@ -1,16 +1,20 @@
 package com.ixi_U.plan.controller;
 
+import com.ixi_U.common.exception.GeneralException;
 import com.ixi_U.plan.dto.response.SortedPlanResponse;
+import com.ixi_U.plan.exception.PlanException;
 import com.ixi_U.plan.service.PlanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/plans")
 @RequiredArgsConstructor
@@ -19,12 +23,17 @@ public class PlanController {
     private final PlanService planService;
 
     @GetMapping
-    public ResponseEntity<SortedPlanResponse> getPlans(@RequestParam(defaultValue = "10") int size,
+    public ResponseEntity<SortedPlanResponse> getPlans(
+            @RequestParam(defaultValue = "10") int size,
             @RequestParam String planTypeStr,
             @RequestParam(defaultValue = "PRIORITY") String planSortOptionStr,
             @RequestParam(required = false) String searchKeyword,
             @RequestParam(required = false) String planId,
             @RequestParam(required = false) Integer cursorSortValue) {
+
+        if (size < 1) {
+            throw new GeneralException(PlanException.INVALID_PARAMETER);
+        }
 
         Pageable pageable = PageRequest.ofSize(size);
         SortedPlanResponse response = planService.findPlans(pageable, planTypeStr,
