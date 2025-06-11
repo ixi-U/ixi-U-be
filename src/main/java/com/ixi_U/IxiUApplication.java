1130dc9fec6167737c35dd800c55e446f918738a
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-11 11:24:17 +0900

    refactor : SCRUM-21 @EnableNeo4jAuditing가 중복 선언되는 것 수정

--- src/main/java/com/ixi_U/IxiUApplication.java
+++ src/main/java/com/ixi_U/IxiUApplication.java
@@ -2,9 +2,7 @@ package com.ixi_U;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
 
-@EnableNeo4jAuditing
 @SpringBootApplication
 public class IxiUApplication {
 
