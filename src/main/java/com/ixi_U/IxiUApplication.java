5b07df549179f126d1d3c785b607e0bdaa4696e4
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-11 10:38:52 +0900

    feat : SCRUM-21 BaseEntity의 @CreatedDate, @LastModifiedDate가 실제로 동작하도록 메인 어플리케이션에 @EnableNeo4jAuditing 추가

--- src/main/java/com/ixi_U/IxiUApplication.java
+++ src/main/java/com/ixi_U/IxiUApplication.java
@@ -2,12 +2,14 @@ package com.ixi_U;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
 
+@EnableNeo4jAuditing
 @SpringBootApplication
 public class IxiUApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IxiUApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IxiUApplication.class, args);
+    }
 
 }
