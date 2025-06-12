c59ff2d854b703f8842c97b34bf337faa57cf314
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-12 11:38:13 +0900

    feat : SCRUM-21 TimeStamp 타임존 명시

--- src/main/java/com/ixi_U/IxiUApplication.java
+++ src/main/java/com/ixi_U/IxiUApplication.java
@@ -1,5 +1,7 @@
 package com.ixi_U;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class IxiUApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IxiUApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
