f786a562fe1bfb4f194e2b8329ef84229e169fb0
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-12 15:27:25 +0900

    test : SCRUM-21 요금제 구독 시 createdAt 세팅 되는 테스트 추가

--- src/test/java/com/ixi_U/common/entity/BaseEntityAuditingTest.java
+++ src/test/java/com/ixi_U/common/entity/BaseEntityAuditingTest.java
@@ -3,6 +3,9 @@ package com.ixi_U.common.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.ixi_U.common.AbstractNeo4jContainer;
+import com.ixi_U.plan.entity.Plan;
+import com.ixi_U.plan.repository.PlanRepository;
+import com.ixi_U.user.entity.Subscribed;
 import com.ixi_U.user.entity.User;
 import com.ixi_U.user.repository.UserRepository;
 import com.ixi_U.user.service.UserService;
@@ -21,11 +24,15 @@ class BaseEntityAuditingTest extends AbstractNeo4jContainer {
 
     @Autowired
     UserRepository userRepository;
+
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PlanRepository planRepository;
+
     @Test
-    @DisplayName("엔티티 저장 시 createdAt, updatedAt이 세팅된다")
+    @DisplayName("유저 엔티티 저장 시 유저에 createdAt, updatedAt이 세팅된다")
     void testCreatedAndUpdatedAtSetOnSave() {
         // given & when
         User savedUser = userRepository.save(User.of("홍길동", "hong@example.com", "KAKAO"));
@@ -33,6 +40,28 @@ class BaseEntityAuditingTest extends AbstractNeo4jContainer {
         // then
         assertThat(savedUser.getCreatedAt()).isNotNull();
         assertThat(savedUser.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("유저가 요금제 구독 시 구독에 createdAt이 세팅된다")
+    void testCreatedAndUpdatedAtSetOnSave2() {
+        // given & when
+        User savedUser = userRepository.save(User.of("홍길동", "hong@example.com", "KAKAO"));
+
+        Plan plan = planRepository.save(Plan.of("5G 요금제"));
+
+        Subscribed subscribed = Subscribed.of(plan);
+        savedUser.addSubscribed(subscribed);
+
+        savedUser = userRepository.save(savedUser);
+
+        Subscribed latestSubscribed = savedUser.getSubscribedHistory().stream()
+                .reduce((first, second) -> second)
+                .orElse(null);
+
+        // then
+        assertThat(latestSubscribed).isNotNull();
+        assertThat(latestSubscribed.getCreatedAt()).isNotNull();
     }
 
     @Test
