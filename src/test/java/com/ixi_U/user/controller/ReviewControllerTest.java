f258851d8bd5f0e1e9c6d2f8e3c78002a6e3962a
Jinu Yi <dionisos198@naver.com>
2025-06-12 09:49:11 +0900

    fix: SCRUM-36 ReviewControllerTest CD 오류 해결

--- src/test/java/com/ixi_U/user/controller/ReviewControllerTest.java
+++ src/test/java/com/ixi_U/user/controller/ReviewControllerTest.java
@@ -40,6 +40,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -49,6 +50,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 @WebMvcTest(value = {ReviewController.class})
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@ActiveProfiles("test")
 class ReviewControllerTest {
 
     private static final String REVIEW_URL = "/api/reviews";
