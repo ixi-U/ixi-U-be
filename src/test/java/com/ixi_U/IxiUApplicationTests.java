f85a20ce997cfcc66e007d3dd6a02c19cd0108cd
이진우 <96781019+dionisos198@users.noreply.github.com>
2025-06-12 10:50:44 +0900

    [fix]:[SCRUM-36] CD 과정에서 ReviewControllerTest CD 오류 해결  (#14)
    * feat: SCRUM-16 리뷰 comment 칼럼 추가
    
    * fix: SCRUM-16 구독 엔티티에서 잘못 참조된 Plan 변경
    
    * chore: SCRUM-16 DTO 검증을 위한 validation 의존성 추가
    
    * feat: SCRUM-16 GlobalException valid를 잡기 위한 핸들러 추가
    
    * feat: SCRUM-16 도메인 별 예외 클래스 생성
    
    * feat: SCRUM-16 도메인 별 리포지토리 생성
    
    * feat: SCRUM-16 리뷰 생성
    
    * test: SCRUM-16 ReviewController 테스트 작성
    
    * test: SCRUM-16 ReviewService 테스트 작성
    
    * test: SCRUM-16 ReviewRepository 생성 테스트
    
    * test: SCRUM-16 SubscribedRepository 구독 존재 테스트
    
    * refactor: SCRUM-16 SubscribedRepository 관계 존재 판단 쿼리 수정
    
    * test: SCRUM-16 불필요한 테스트 코드 제거
    
    * test: SCRUM-16 불필요한 의존 제거
    
    * feat: SCRUM-16 리뷰 comment null인지 확인
    
    * feat: SCRUM-16 DTO 데이터 예시 명칭 변경
    
    * test: SCRUM-16 리뷰 저장 검증 로직 추가
    
    * test: SCRUM-16 불필요한 변수 제거
    
    * feat: SCRUM-16 리뷰 리스트 조회
    
    * test: SCRUM-16 ReviewController 조회 테스트 작성
    
    * test: SCRUM-16 ReviewRepository 조회 테스트 작성
    
    * test: SCRUM-16 ReviewService 조회 테스트 작성
    
    * test: SCRUM-16 불필요한 영속화 제거
    
    * test: SCRUM-16 reuse 키워드 제거
    
    * feat: SCRUM-16 index.html 삭제
    
    * feat: SCRUM-16 리뷰 created at 칼럼 추가
    
    * test: SCRUM-16 리뷰 created at 칼럼 추가
    
    * chore: SCRUM-16 rest doc gradle mock mvc 관련 설정 추가
    
    * test: SCRUM-16 rest doc 설정 적용
    
    * chore: SCRUM-16 rest document에 review 저장 api 설정
    
    * test: SCRUM-16 rest document에 review 조회 설정
    
    * chore: SCRUM-16 rest document에 review 조회 설정
    
    * feat: SCRUM-16 test용 yml 설정 복구
    
    * feat: SCRUM-16 review createdAt final 키워드 명시
    
    * test: SCRUM-16 reviewController 불필요한 given 제거
    
    * test: SCRUM-16 reviewController 비즈니스 예외 rest docs 명시
    
    * chore: SCRUM-16 reviewController 비즈니스 예외 rest docs 명시
    
    * test: SCRUM-16 optinal에 대한 검증 present로 변경
    
    * test: SCRUM-16 convention 적용
    
    * feat: SCRUM-16 응답 형식 통일화
    
    * feat: SCRUM-16 convention 적용
    
    * fix: SCRUM-36 ReviewControllerTest CD 오류 해결
    
    * test: SCRUM-36 Neo4J 테스트 버전 변경

--- src/test/java/com/ixi_U/IxiUApplicationTests.java
+++ src/test/java/com/ixi_U/IxiUApplicationTests.java
@@ -17,14 +17,14 @@ class IxiUApplicationTests {
 
     @Container
     private static Neo4jContainer<?> neo4j = new Neo4jContainer<>(
-            DockerImageName.parse("neo4j:4.4"))
-            .withAdminPassword("secret");
+            DockerImageName.parse("neo4j:5.24"))
+            .withAdminPassword("1q2w3e4r");
 
     @DynamicPropertySource
     static void overrideNeo4jProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.neo4j.uri", neo4j::getBoltUrl);
         registry.add("spring.neo4j.authentication.username", () -> "neo4j");
-        registry.add("spring.neo4j.authentication.password", () -> "secret");
+        registry.add("spring.neo4j.authentication.password", () -> "1q2w3e4r");
     }
 
     @Test
