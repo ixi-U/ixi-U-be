ac85e8b6ba7e5394191e44abc3987b30d23ba796
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-12 16:32:46 +0900

    fix : SCRUM-21 PlanCustomRepository extends 하도록 수정

--- src/main/java/com/ixi_U/plan/repository/PlanRepository.java
+++ src/main/java/com/ixi_U/plan/repository/PlanRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanRepository extends Neo4jRepository<Plan, String> {
+public interface PlanRepository extends Neo4jRepository<Plan, String>, PlanCustomRepository {
 
 }
