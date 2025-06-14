c5ef105113910164545920d1e543b2c91a902199
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-14 22:18:18 +0900

    test: SCRUM-34 생성자 메서드 명 수정

--- src/test/java/com/ixi_U/chatbot/testutil/TestDataFactory.java
+++ src/test/java/com/ixi_U/chatbot/testutil/TestDataFactory.java
@@ -17,10 +17,10 @@ public class TestDataFactory {
         SingleBenefitRequest sbr = SingleBenefitRequest.create(
                 UUID.randomUUID().toString(), "테스트 단일 혜택", "기본 단일 혜택", BenefitType.DEVICE);
 
-        BundledBenefitRequest bbr = BundledBenefitRequest.of(
+        BundledBenefitRequest bbr = BundledBenefitRequest.create(
                 UUID.randomUUID().toString(), "테스트 번들 혜택", "기본 번들 혜택", 1, List.of(sbr));
 
-        return PlanDescriptionRequest.of(UUID.randomUUID().toString(), "테스트 요금제", "기본 요금제", 100,
+        return PlanDescriptionRequest.create(UUID.randomUUID().toString(), "테스트 요금제", "기본 요금제", 100,
                 59000, List.of(bbr), List.of(sbr));
     }
 
@@ -34,10 +34,10 @@ public class TestDataFactory {
         SingleBenefitRequest sbr2 = SingleBenefitRequest.create(
                 UUID.randomUUID().toString(), null, null, BenefitType.DEVICE);
 
-        BundledBenefitRequest bbr = BundledBenefitRequest.of(
+        BundledBenefitRequest bbr = BundledBenefitRequest.create(
                 UUID.randomUUID().toString(), "테스트 번들 혜택", "기본 번들 혜택", 1, List.of(sbr1));
 
-        return PlanDescriptionRequest.of(UUID.randomUUID().toString(), "테스트 요금제", "기본 요금제", 100,
+        return PlanDescriptionRequest.create(UUID.randomUUID().toString(), "테스트 요금제", "기본 요금제", 100,
                 59000, List.of(bbr), List.of(sbr2));
     }
 }
