a8ac0ef2974425f94e0abec816cc21e4c66d6a7d
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-14 23:28:10 +0900

    refactor: SCRUM-34 필드명 수정

--- src/main/java/com/ixi_U/plan/entity/PlanState.java
+++ src/main/java/com/ixi_U/plan/entity/PlanState.java
@@ -3,14 +3,14 @@ package com.ixi_U.plan.entity;
 import lombok.Getter;
 
 @Getter
-public enum State {
+public enum PlanState {
 
     ABLE("ABLE"),
     DISABLE("DISABLE");
 
     private final String state;
 
-    State(String state){
+    PlanState(String state){
 
         this.state = state;
     }
