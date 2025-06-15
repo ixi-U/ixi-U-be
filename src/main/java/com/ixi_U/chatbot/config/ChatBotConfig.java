b38f8bd4aef99ed035e0cac860270cd1e87085ff
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-15 22:05:21 +0900

    feat: SCRUM-27 추천 챗봇 config 추가

--- src/main/java/com/ixi_U/chatbot/config/ChatBotConfig.java
+++ src/main/java/com/ixi_U/chatbot/config/ChatBotConfig.java
@@ -1,25 +1,31 @@
 package com.ixi_U.chatbot.config;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.neo4j.Neo4jChatMemoryRepository;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @RequiredArgsConstructor
 public class ChatBotConfig {
 
     private static final String PROMPT_PATH = "classpath:/prompts/description.txt";
+    private static final String RECOMMEND_PROMPT = "classpath:/prompts/recommend";
     private final Neo4jChatMemoryRepository neo4jChatMemoryRepository;
     private final ResourceLoader resourceLoader;
+    private final VectorStore vectorStore;
 
     private String loadPrompt(String path) {
         try {
@@ -30,7 +36,6 @@ public class ChatBotConfig {
             throw new RuntimeException("프롬프트 파일 로드 실패 : " + path, e);
         }
     }
-
 
     @Bean
     public ChatClient descriptionClient(ChatClient.Builder chatClientBuilder) {
@@ -43,14 +48,22 @@ public class ChatBotConfig {
     }
 
     /**
-     * ChatClient Build
+     * Recommend Build
      */
     @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+    public ChatClient recommendClient(ChatClient.Builder chatClientBuilder) {
+
+//        String prompt = loadPrompt(RECOMMEND_PROMPT);
 
         return chatClientBuilder
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(chatMemory()).build()
+                        QuestionAnswerAdvisor.builder(vectorStore)
+                                .searchRequest(SearchRequest.builder()
+                                        .similarityThreshold(0.1)
+                                        .topK(5)
+                                        .build())
+                                .build()
                 )
                 .build();
     }
