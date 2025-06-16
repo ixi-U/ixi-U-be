ddd841c42b0134dcf2292c57a454a8cc3fc6d403
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-06-16 12:42:05 +0900

    [fix]:[SCRUM-59] 요금제 목록 조회 기능 수정 (#31)
    * fix: SCRUM-59 bundledBenefits 필드 추가
    
    * fix: SCRUM-59 bundledBenefits 조회 추가
    
    * test: SCRUM-59 PlanSummaryDto 연관 테스트 수정

--- src/test/java/com/ixi_U/plan/controller/PlanControllerTest.java
+++ src/test/java/com/ixi_U/plan/controller/PlanControllerTest.java
@@ -1,17 +1,38 @@
 package com.ixi_U.plan.controller;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ixi_U.benefit.entity.BenefitType;
 import com.ixi_U.common.exception.GeneralException;
 import com.ixi_U.plan.dto.PlanSummaryDto;
 import com.ixi_U.plan.dto.request.GetPlansRequest;
 import com.ixi_U.plan.dto.request.SavePlanRequest;
-import com.ixi_U.plan.dto.response.*;
+import com.ixi_U.plan.dto.response.BundledBenefitResponse;
+import com.ixi_U.plan.dto.response.PlanDetailResponse;
+import com.ixi_U.plan.dto.response.PlanEmbeddedResponse;
+import com.ixi_U.plan.dto.response.SingleBenefitResponse;
+import com.ixi_U.plan.dto.response.SortedPlanResponse;
 import com.ixi_U.plan.entity.PlanType;
 import com.ixi_U.plan.exception.PlanException;
 import com.ixi_U.plan.service.PlanService;
 import com.ixi_U.util.constants.TestConstants;
 import jakarta.servlet.Filter;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -33,24 +54,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
-
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.IntStream;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @WebMvcTest(PlanController.class)
@@ -122,14 +125,18 @@ class PlanControllerTest {
                     .andExpect(jsonPath("$.description").value(description))
                     .andExpect(jsonPath("$.metaData.id").value(TestConstants.createPlanId()))
                     .andExpect(jsonPath("$.metaData.name").value(request.name()))
-                    .andExpect(jsonPath("$.metaData.mobileDataLimitMb").value(request.mobileDataLimitMb()))
+                    .andExpect(jsonPath("$.metaData.mobileDataLimitMb").value(
+                            request.mobileDataLimitMb()))
                     .andExpect(jsonPath("$.metaData.monthlyPrice").value(request.monthlyPrice()))
                     .andExpect(jsonPath("$.metaData.bundledBenefitNames").isArray())
-                    .andExpect(jsonPath("$.metaData.bundledBenefitNames", hasSize(bundledBenefitNames.size())))
+                    .andExpect(jsonPath("$.metaData.bundledBenefitNames",
+                            hasSize(bundledBenefitNames.size())))
                     .andExpect(jsonPath("$.metaData.singleBenefitNames").isArray())
-                    .andExpect(jsonPath("$.metaData.singleBenefitNames", hasSize(singleBenefitNames.size())))
+                    .andExpect(jsonPath("$.metaData.singleBenefitNames",
+                            hasSize(singleBenefitNames.size())))
                     .andExpect(jsonPath("$.metaData.singleBenefitTypes").isArray())
-                    .andExpect(jsonPath("$.metaData.singleBenefitTypes", hasSize(singleBenefitTypes.size())))
+                    .andExpect(jsonPath("$.metaData.singleBenefitTypes",
+                            hasSize(singleBenefitTypes.size())))
                     .andDo(document("plans-save"));
         }
     }
@@ -143,9 +150,9 @@ class PlanControllerTest {
 
             // given
             PlanSummaryDto dto1 = new PlanSummaryDto("1", "요금제1", 10000, 2000, 300, 200, 29000, 5,
-                    List.of());
+                    List.of(), List.of());
             PlanSummaryDto dto2 = new PlanSummaryDto("2", "요금제2", 8000, 1000, 200, 100, 19000, 3,
-                    List.of());
+                    List.of(), List.of());
             SortedPlanResponse response =
                     new SortedPlanResponse(new SliceImpl<>(List.of(dto1, dto2)), "2", 3);
 
@@ -175,7 +182,7 @@ class PlanControllerTest {
 
             // given
             PlanSummaryDto dto2 = new PlanSummaryDto("2", "요금제2", 8000, 1000, 200, 100, 19000, 3,
-                    List.of());
+                    List.of(), List.of());
             SortedPlanResponse response =
                     new SortedPlanResponse(new SliceImpl<>(List.of(dto2)), "2", 3);
 
