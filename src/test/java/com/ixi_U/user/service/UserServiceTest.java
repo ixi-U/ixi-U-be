c7639183a89842d9288454f97978c1daa88b6a32
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-16 09:52:17 +0900

    test : SCRUM-48 회원 삭제 시 SecurityContext의 회원 ID로 진행하는 테스트 추가

--- src/test/java/com/ixi_U/user/service/UserServiceTest.java
+++ src/test/java/com/ixi_U/user/service/UserServiceTest.java
@@ -3,6 +3,9 @@ package com.ixi_U.user.service;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.ixi_U.auth.dto.CustomOAuth2User;
+import com.ixi_U.user.entity.User;
+import com.ixi_U.user.entity.UserRole;
 import com.ixi_U.user.repository.SubscribedRepository;
 import com.ixi_U.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -25,7 +30,7 @@ class UserServiceTest {
     private UserService userService;
 
     @Test
-    @DisplayName("카카오 소셜 유저 탈퇴 시 유저와 구독 정보 삭제 확인")
+    @DisplayName("카카오 소셜 회원 탈퇴 시 회원과 구독 정보 삭제 확인")
     void deleteKakaoUserAndSubscriptions() {
         // given
         String userId = "kakao123";
@@ -38,5 +43,34 @@ class UserServiceTest {
         verify(subscribedRepository).deleteAllByUserId(userId);
         verify(userRepository).deleteById(userId);
     }
-}
 
+    @Test
+    @DisplayName("@AuthenticationPrincipal UserId를 통한 회원 탈퇴 로직 확인")
+    void deleteUser_withCustomOAuth2User() {
+        // given
+        User dummyUser = User.of("nickname", "email@example.com", "kakao", 123456L,
+                UserRole.ROLE_USER);
+        when(userRepository.save(dummyUser)).thenReturn(dummyUser);
+        when(userRepository.existsById(dummyUser.getId())).thenReturn(true);
+
+        userRepository.save(dummyUser);
+
+        CustomOAuth2User customUser = new CustomOAuth2User(
+                dummyUser.getName(),
+                dummyUser.getId(),
+                dummyUser.getUserRole()
+        );
+
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(dummyUser.getId(), null,
+                        customUser.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        // when
+        userService.deleteUserById(customUser.getUserId());
+
+        // then
+        verify(userRepository).deleteById(dummyUser.getId());
+    }
+
+}
