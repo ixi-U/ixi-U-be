2e44f688b41fcb127c8e59376e23e67e75c6729b
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-16 14:49:06 +0900

    feat: SCRUM-27 추천 챗봇 설정 추가

--- src/main/java/com/ixi_U/chatbot/config/ChatBotConfig.java
+++ src/main/java/com/ixi_U/chatbot/config/ChatBotConfig.java
@@ -1,14 +1,11 @@
 package com.ixi_U.chatbot.config;
 
+import com.ixi_U.chatbot.tool.RecommendTool;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.neo4j.Neo4jChatMemoryRepository;
-import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -21,13 +18,14 @@ import java.nio.charset.StandardCharsets;
 @RequiredArgsConstructor
 public class ChatBotConfig {
 
-    private static final String PROMPT_PATH = "classpath:/prompts/description.txt";
+    private static final String PROMPT_PATH = "classpath:/prompts/description";
     private static final String RECOMMEND_PROMPT = "classpath:/prompts/recommend";
+
     private final Neo4jChatMemoryRepository neo4jChatMemoryRepository;
     private final ResourceLoader resourceLoader;
-    private final VectorStore vectorStore;
 
     private String loadPrompt(String path) {
+
         try {
             Resource resource = resourceLoader.getResource(path);
             byte[] bytes = resource.getInputStream().readAllBytes();
@@ -51,20 +49,13 @@ public class ChatBotConfig {
      * Recommend Build
      */
     @Bean
-    public ChatClient recommendClient(ChatClient.Builder chatClientBuilder) {
+    public ChatClient recommendClient(ChatClient.Builder chatClientBuilder, RecommendTool recommendTool) {
 
-//        String prompt = loadPrompt(RECOMMEND_PROMPT);
+        String prompt = loadPrompt(RECOMMEND_PROMPT);
 
         return chatClientBuilder
-                .defaultAdvisors(
-                        MessageChatMemoryAdvisor.builder(chatMemory()).build(),
-                        QuestionAnswerAdvisor.builder(vectorStore)
-                                .searchRequest(SearchRequest.builder()
-                                        .similarityThreshold(0.1)
-                                        .topK(5)
-                                        .build())
-                                .build()
-                )
+                .defaultSystem(prompt)
+                .defaultTools(recommendTool)
                 .build();
     }
 
