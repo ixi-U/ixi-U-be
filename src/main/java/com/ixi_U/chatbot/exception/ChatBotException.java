6b3a43594b48a65bdd95b7c5309e555cfd67e9d4
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-16 17:23:03 +0900

    feat: SCRUM-27 챗봇 예외 추가

--- src/main/java/com/ixi_U/chatbot/exception/ChatBotException.java
+++ src/main/java/com/ixi_U/chatbot/exception/ChatBotException.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ChatBotException implements BaseException {
 
-    CHAT_BOT_BAD_RESPONSE(HttpStatus.SERVICE_UNAVAILABLE, "LLM 응답이 유효하지 않습니다.");
+    CHAT_BOT_BAD_RESPONSE(HttpStatus.SERVICE_UNAVAILABLE, "LLM 응답이 유효하지 않습니다."),
+    CHAT_BOT_PRICE_EXTRACTOR_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "가격 키워드 추출에서 오류가 발생했습니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
