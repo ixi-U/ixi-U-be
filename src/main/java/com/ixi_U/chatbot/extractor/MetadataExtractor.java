ae3ffcc22abe15b43d8eb3fcf361beeeb5e5bf79
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-17 13:46:41 +0900

    feat: SCRUM-27 키워드 추출 기능 삭제

--- src/main/java/com/ixi_U/chatbot/extractor/MetadataExtractor.java
+++
@@ -1,8 +0,0 @@
-package com.ixi_U.chatbot.extractor;
-
-public interface MetadataExtractor {
-
-    FilterConditions extractFilters(String query);
-
-    String generateCleanQuery(String originalQuery, FilterConditions filters);
-}
--- src/main/java/com/ixi_U/chatbot/extractor/Operator.java
+++
@@ -1,9 +0,0 @@
-package com.ixi_U.chatbot.extractor;
-
-public enum Operator {
-    LESS_THAN,
-    LESS_THAN_OR_EQUAL,
-    GREATER_THAN,
-    GREATER_THAN_OR_EQUAL,
-    EQUAL
-}
