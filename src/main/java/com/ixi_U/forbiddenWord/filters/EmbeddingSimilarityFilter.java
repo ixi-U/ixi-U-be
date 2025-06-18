260c36424dba14a06ecb551859bab1384e47dcfe
yereumi <lovelyyeji01@gmail.com>
2025-06-18 21:47:16 +0900

    fix: SCRUM-94 test 프로파일에서는 로직 실행을 건너뛰도록 조건 수정

--- src/main/java/com/ixi_U/forbiddenWord/filters/EmbeddingSimilarityFilter.java
+++ src/main/java/com/ixi_U/forbiddenWord/filters/EmbeddingSimilarityFilter.java
@@ -13,6 +13,7 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component("embeddingSimilarityFilter")
@@ -23,39 +24,50 @@ public class EmbeddingSimilarityFilter implements ForbiddenWordFilter, Applicati
     @Qualifier("forbiddenVectorStore")
     private final VectorStore vectorStore;
     private final KoreanAnalyzeService koreanAnalyzeService;
+    private final Environment environment;
 
     public void run(ApplicationArguments args) throws Exception {
 
-        // 벡터저장소에 존재할 경우 금칙어 데이터 저장 안함
-//        List<Document> docsToAdd = forbiddenWordLoader.getForbiddenWords().stream()
-//                .filter(w -> vectorStore.similaritySearch(
-//                                SearchRequest.builder()
-//                                        .query(w)
-//                                        .topK(1)
-//                                        .filterExpression("id == '" + w + "'")
-//                                        .build()
-//                        ).isEmpty()
-//                )
-//                .map(w -> new Document(
-//                        w,
-//                        Map.of("id", w, "type", "EmbeddedForbidden")
-//                ))
-//                .toList();
-//
-//        System.out.println("중간");
+        if (System.getProperty("spring.profiles.active") == null) {
+            System.setProperty("spring.profiles.active", "default");
+        }
 
-        // 벡터 저장소에 존재 여부 상관없이 데이터 저장
+        String[] activeProfiles = environment.getActiveProfiles();
+        for (String profile : activeProfiles) {
+            if ("test".equalsIgnoreCase(profile)) {
+                return;
+            }
+        }
+
+        // 벡터저장소에 존재할 경우 금칙어 데이터 저장 안함
         List<Document> docsToAdd = forbiddenWordLoader.getForbiddenWords().stream()
+                .filter(w -> vectorStore.similaritySearch(
+                                SearchRequest.builder()
+                                        .query(w)
+                                        .topK(1)
+                                        .filterExpression("id == '" + w + "'")
+                                        .build()
+                        ).isEmpty()
+                )
                 .map(w -> new Document(
                         w,
                         Map.of("id", w, "type", "EmbeddedForbidden")
                 ))
                 .toList();
 
-        System.out.println("docsToAdd.size() = " + docsToAdd.size());
-        if (!docsToAdd.isEmpty()) {
-            vectorStore.add(docsToAdd);
-        }
+
+        // 벡터 저장소에 존재 여부 상관없이 데이터 저장
+//        List<Document> docsToAdd = forbiddenWordLoader.getForbiddenWords().stream()
+//                .map(w -> new Document(
+//                        w,
+//                        Map.of("id", w, "type", "EmbeddedForbidden")
+//                ))
+//                .toList();
+//
+//        System.out.println("docsToAdd.size() = " + docsToAdd.size());
+//        if (!docsToAdd.isEmpty()) {
+//            vectorStore.add(docsToAdd);
+//        }
     }
 
     public List<Document> findSimilarForbiddenWords(String text) {
