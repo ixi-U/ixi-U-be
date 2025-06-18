0c0c85a537ea22eaf594ec8e4eae7a62802dbdec
yereumi <lovelyyeji01@gmail.com>
2025-06-18 17:34:35 +0900

    chore: SCRUM-94 금칙어 Controller 추가

--- src/main/java/com/ixi_U/forbiddenWord/ForbiddenWordController.java
+++ src/main/java/com/ixi_U/forbiddenWord/ForbiddenWordController.java
@@ -1,15 +1,16 @@
 package com.ixi_U.forbiddenWord;
 
 import com.ixi_U.forbiddenWord.filters.AhoCorasickFilter;
+import com.ixi_U.forbiddenWord.filters.EmbeddingSimilarityFilter;
 import com.ixi_U.forbiddenWord.filters.LevenshteinThresholdFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@Controller
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/forbidden-word")
@@ -18,6 +19,7 @@ public class ForbiddenWordController {
     private final ForbiddenWordLoader forbiddenWordLoader;
     private final AhoCorasickFilter ahoCorasickFilter;
     private final LevenshteinThresholdFilter levenshteinFilter;
+    private final EmbeddingSimilarityFilter embeddingSimilarityFilter;
     private final ReviewFilter reviewFilter;
     private final ChatbotFilter chatbotFilter;
 
@@ -27,7 +29,7 @@ public class ForbiddenWordController {
         System.out.println("input = " + input);
         for (String word : forbiddenWordLoader.getForbiddenWords()) {
             if (input.contains(word)) {
-                
+
                 return true;
             }
         }
@@ -46,6 +48,27 @@ public class ForbiddenWordController {
 
         System.out.println("input = " + input);
         return levenshteinFilter.matches(input);
+    }
+
+    @GetMapping("/embedding")
+    public boolean embeddingV(@RequestParam String input) {
+
+        System.out.println("input = " + input);
+        return embeddingSimilarityFilter.matches(input);
+    }
+
+    @GetMapping("/contains-embedding")
+    public boolean containsEmbeddingV(@RequestParam String input) {
+
+        System.out.println("input = " + input);
+
+        for (String word : forbiddenWordLoader.getForbiddenWords()) {
+            if (input.contains(word)) {
+
+                return true;
+            }
+        }
+        return embeddingSimilarityFilter.matches(input);
     }
 
     @GetMapping("/chatbot")
