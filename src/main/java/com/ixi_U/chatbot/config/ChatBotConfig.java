4f3e2b13a8d24c0872c1facf758450e3b3b24212
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-06-18 10:27:37 +0900

    [feat]:[SCRUM-66] 금칙어 시스템 최종 구현 (#42)
    * feat: SCRUM-66 ForbiddenWordFilter 인터페이스 추가
    
    * feat: SCRUM-66 아호코라식 필터 추가
    
    * feat: SCRUM-66 레벤슈타인 필터 추가
    
    * feat: SCRUM-66 RAG 필터 추가
    
    * feat: SCRUM-66 LLM 필터 추가
    
    * feat: SCRUM-66 챗봇 필터 추가
    
    * feat: SCRUM-66 리뷰 필터 추가
    
    * feat: SCRUM-66 금칙어 로더 추가
    
    * feat: SCRUM-66 JMeter 테스트를 위한 Controller 추가
    
    * feat: SCRUM-66 금칙어 일부 제거
    
    * chore: SCRUM-66 사용하지 않는 파일 제거
    
    * chore: SCRUM-66 파일 구조 변경
    
    * style: SCRUM-66 공백 제거
    
    * style: SCRUM-66 공백 제거
    
    * chore: SCRUM-66 공백 파일 제거
    
    * chore: SCRUM-66 import문 추가
    
    * style: SCRUM-66 공백 추가
    
    * fix: SCRUM-66 vector store 수정

--- src/main/java/com/ixi_U/chatbot/config/ChatBotConfig.java
+++ src/main/java/com/ixi_U/chatbot/config/ChatBotConfig.java
@@ -1,6 +1,8 @@
 package com.ixi_U.chatbot.config;
 
 import com.ixi_U.chatbot.tool.RecommendTool;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -12,9 +14,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 @Configuration
 @RequiredArgsConstructor
 public class ChatBotConfig {
@@ -22,13 +21,11 @@ public class ChatBotConfig {
     private static final String EMBEDDING_PROMPT = "classpath:/prompts/embedding-prompt.txt";
     private static final String RECOMMEND_PROMPT = "classpath:/prompts/recommending-prompt.txt";
     private static final String FILTER_EXPRESSION_PROMPT = "classpath:/prompts/filter-expression-prompt.txt";
-
-    private final Neo4jChatMemoryRepository neo4jChatMemoryRepository;
-    private final ResourceLoader resourceLoader;
-
     private static final String decisionForbiddenWordPrompt = """
             다음 문장이 욕설이나 부적절한 표현, LG U+가 아닌 다른 통신사에 관한 내용을 포함하고 있습니까? "예" 또는 "아니오"로만 대답하세요.
             """;
+    private final Neo4jChatMemoryRepository neo4jChatMemoryRepository;
+    private final ResourceLoader resourceLoader;
 
     private String loadPrompt(String path) {
 
@@ -63,7 +60,8 @@ public class ChatBotConfig {
      * Recommend Build
      */
     @Bean
-    public ChatClient recommendClient(ChatClient.Builder chatClientBuilder, RecommendTool recommendTool) {
+    public ChatClient recommendClient(ChatClient.Builder chatClientBuilder,
+            RecommendTool recommendTool) {
 
         String prompt = loadPrompt(RECOMMEND_PROMPT);
 
