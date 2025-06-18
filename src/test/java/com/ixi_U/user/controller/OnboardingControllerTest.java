d324cc84b8ad0996911b9d3031aa171f0506faf6
Minjae An <101340860+Minjae-An@users.noreply.github.com>
2025-06-18 15:02:14 +0900

    [fix]:[SCRUM-63] 잘못된 테스트 설정 수정 (#45)
    * feat: SCRUM-63 allowed origin에 프론트 도메인 추가
    
    * feat: SCRUM-63 allowed origin에 프론트 도메인 추가
    
    * test: SCRUM-63 테스트 프로필 설정

--- src/test/java/com/ixi_U/user/controller/OnboardingControllerTest.java
+++ src/test/java/com/ixi_U/user/controller/OnboardingControllerTest.java
@@ -1,12 +1,18 @@
 package com.ixi_U.user.controller;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ixi_U.user.dto.request.OnboardingRequest;
 import com.ixi_U.user.service.UserService;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,16 +20,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@ActiveProfiles("test")
 @WebMvcTest(UserController.class)
 class OnboardingControllerTest {
 
