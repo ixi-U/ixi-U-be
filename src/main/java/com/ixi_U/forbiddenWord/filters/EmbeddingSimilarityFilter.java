b65f983176cd93cbd67877bc4e4e1d7296933c3d
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-06-21 14:10:08 +0900

    fix: SCRUM-114 금칙어 임베딩 로직 제거 (#61)

--- src/main/java/com/ixi_U/forbiddenWord/filters/EmbeddingSimilarityFilter.java
+++ src/main/java/com/ixi_U/forbiddenWord/filters/EmbeddingSimilarityFilter.java
@@ -1,9 +1,7 @@
 package com.ixi_U.forbiddenWord.filters;
 
-import com.ixi_U.forbiddenWord.ForbiddenWordLoader;
 import com.ixi_U.forbiddenWord.KoreanAnalyzeService;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
@@ -11,69 +9,21 @@ import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component("embeddingSimilarityFilter")
 @RequiredArgsConstructor
-public class EmbeddingSimilarityFilter implements ForbiddenWordFilter, ApplicationRunner {
+public class EmbeddingSimilarityFilter implements ForbiddenWordFilter {
 
-    private final ForbiddenWordLoader forbiddenWordLoader;
     @Qualifier("forbiddenVectorStore")
     private final VectorStore vectorStore;
     private final KoreanAnalyzeService koreanAnalyzeService;
-    private final Environment environment;
-
-    public void run(ApplicationArguments args) throws Exception {
-
-        if (System.getProperty("spring.profiles.active") == null) {
-            System.setProperty("spring.profiles.active", "default");
-        }
-
-        String[] activeProfiles = environment.getActiveProfiles();
-        for (String profile : activeProfiles) {
-            if ("test".equalsIgnoreCase(profile)) {
-                return;
-            }
-        }
-
-        // 벡터저장소에 존재할 경우 금칙어 데이터 저장 안함
-        List<Document> docsToAdd = forbiddenWordLoader.getForbiddenWords().stream()
-                .filter(w -> vectorStore.similaritySearch(
-                                SearchRequest.builder()
-                                        .query(w)
-                                        .topK(1)
-                                        .filterExpression("id == '" + w + "'")
-                                        .build()
-                        ).isEmpty()
-                )
-                .map(w -> new Document(
-                        w,
-                        Map.of("id", w, "type", "EmbeddedForbidden")
-                ))
-                .toList();
-
-
-        // 벡터 저장소에 존재 여부 상관없이 데이터 저장
-//        List<Document> docsToAdd = forbiddenWordLoader.getForbiddenWords().stream()
-//                .map(w -> new Document(
-//                        w,
-//                        Map.of("id", w, "type", "EmbeddedForbidden")
-//                ))
-//                .toList();
-//
-//        System.out.println("docsToAdd.size() = " + docsToAdd.size());
-//        if (!docsToAdd.isEmpty()) {
-//            vectorStore.add(docsToAdd);
-//        }
-    }
 
     public List<Document> findSimilarForbiddenWords(String text) {
 
         List<String> chunks = koreanAnalyzeService.getAnalyze(text);
 
+        System.out.println("chunks = " + chunks);
         Set<Document> results = ConcurrentHashMap.newKeySet();
 
         chunks.parallelStream().forEach(chunk -> {
@@ -81,7 +31,7 @@ public class EmbeddingSimilarityFilter implements ForbiddenWordFilter, Applicati
                     SearchRequest.builder()
                             .query(chunk)
                             .topK(3)
-                            .similarityThreshold(0.75)
+                            .similarityThreshold(0.8)
                             .filterExpression("type == 'EmbeddedForbidden'")
                             .build()
             );
@@ -89,6 +39,8 @@ public class EmbeddingSimilarityFilter implements ForbiddenWordFilter, Applicati
                 results.addAll(found);
             }
         });
+
+        System.out.println("results = " + results);
 
         return List.copyOf(results);
     }
@@ -98,4 +50,5 @@ public class EmbeddingSimilarityFilter implements ForbiddenWordFilter, Applicati
 
         return !findSimilarForbiddenWords(text).isEmpty();
     }
+
 }
