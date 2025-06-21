992fab8fa4d3be94b6fdaa95135921413157b511
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-06-21 22:43:35 +0900

    feat: SCRUM-110 임베딩 모델 차원수 로그 출력 기능 추가

--- src/main/java/com/ixi_U/chatbot/config/VectorStoreConfig.java
+++ src/main/java/com/ixi_U/chatbot/config/VectorStoreConfig.java
@@ -1,5 +1,6 @@
 package com.ixi_U.chatbot.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.neo4j.driver.Driver;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -7,12 +8,15 @@ import org.springframework.ai.vectorstore.neo4j.Neo4jVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class VectorStoreConfig {
 
     @Bean
     public VectorStore forbiddenVectorStore(Driver driver, EmbeddingModel embeddingModel) {
 
+        log.info("임베딩 모델 차원수 확인 : {}",embeddingModel.dimensions());
+        
         return Neo4jVectorStore.builder(driver, embeddingModel)
                 .databaseName("neo4j")
                 .indexName("embedded-forbidden-index")
