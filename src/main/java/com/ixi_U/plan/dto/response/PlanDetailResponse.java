bcea956fcddf32dcd941ef02bfc0efbf9b6ec3cf
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-23 09:57:34 +0900

    feat : SCRUM-132 @GetMapping("/plans/details/{planId}") 요청에서 PlanState를 반환하도록 PlanDetailResponse에 추가

--- src/main/java/com/ixi_U/plan/dto/response/PlanDetailResponse.java
+++ src/main/java/com/ixi_U/plan/dto/response/PlanDetailResponse.java
@@ -1,13 +1,14 @@
 package com.ixi_U.plan.dto.response;
 
 import com.ixi_U.plan.entity.Plan;
+import com.ixi_U.plan.entity.PlanState;
 import java.util.List;
 
-public record PlanDetailResponse(String name, Integer mobileDataLimitMb, Integer sharedMobileDataLimitMb,
-                                 Integer callLimitMinutes, Integer messageLimit, Integer monthlyPrice,
-                                 String planType, String usageCautions,
-                                 Integer mobileDataThrottleSpeedKbps, Integer minAge, Integer maxAge,
-                                 Double pricePerKb, String etcInfo,
+public record PlanDetailResponse(String name, PlanState planState, Integer mobileDataLimitMb,
+                                 Integer sharedMobileDataLimitMb, Integer callLimitMinutes,
+                                 Integer messageLimit, Integer monthlyPrice, String planType,
+                                 String usageCautions, Integer mobileDataThrottleSpeedKbps,
+                                 Integer minAge, Integer maxAge, Double pricePerKb, String etcInfo,
                                  List<BundledBenefitResponse> bundledBenefits,
                                  List<SingleBenefitResponse> singleBenefits) {
 
@@ -21,12 +22,12 @@ public record PlanDetailResponse(String name, Integer mobileDataLimitMb, Integer
                 .map(SingleBenefitResponse::from)
                 .toList();
 
-        return new PlanDetailResponse(plan.getName(), plan.getMobileDataLimitMb(),
-                plan.getSharedMobileDataLimitMb(),
+        return new PlanDetailResponse(plan.getName(), plan.getPlanState(),
+                plan.getMobileDataLimitMb(), plan.getSharedMobileDataLimitMb(),
                 plan.getCallLimitMinutes(), plan.getMessageLimit(), plan.getMonthlyPrice(),
                 plan.getPlanType().name(), plan.getUsageCautions(),
-                plan.getMobileDataThrottleSpeedKbps(),
-                plan.getMinAge(), plan.getMaxAge(), plan.getPricePerKb(), plan.getEtcInfo(),
+                plan.getMobileDataThrottleSpeedKbps(), plan.getMinAge(),
+                plan.getMaxAge(), plan.getPricePerKb(), plan.getEtcInfo(),
                 bundledBenefits, singleBenefits);
     }
 }
